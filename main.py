ad772c9bb598f8b56eb2576d9475b709d8d843d0
Proksima1 <konstant.2015b@mail.ru>
2021-02-16 16:36:23 +0300

    Делаю движение по стрелкам

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import requests
 from PyQt5 import uic, QtCore
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit
+from modules import *
 
 
 class Main(QMainWindow):
@@ -12,6 +13,7 @@ class Main(QMainWindow):
         uic.loadUi('map_api.ui', self)
         self.pushButton.clicked.connect(self.get_map)
         self.map_file = None
+        self.position = [37.615370, 55.756936]
         self.scale = 5
 
     def isEmpty(self, lineEdit: QLineEdit):
@@ -31,20 +33,22 @@ class Main(QMainWindow):
         except TypeError:
             pass
         if not (self.isEmpty(self.lineEdit) and self.isEmpty(self.lineEdit_2)):
-            spis = [','.join(self.lineEdit.text().split(' '))]
+            coord = [','.join(self.lineEdit.text().split(' '))]
+            self.position = coord
+            print(self.position)
             if len(self.lineEdit_2.text().strip().split(' ')) == 1:
-                spis.append(','.join([self.lineEdit_2.text().strip(), self.lineEdit_2.text().strip()]))
+                coord.append(','.join([self.lineEdit_2.text().strip(), self.lineEdit_2.text().strip()]))
                 self.scale = float(self.lineEdit_2.text().strip())
             else:
                 max_scale = str(max(list(map(float, self.lineEdit_2.text().strip().split(' ')))))
                 self.lineEdit_2.setText(max_scale)
                 self.scale = float(max_scale)
-                spis.append(','.join([max_scale, max_scale]))
-            for i in spis:
+                coord.append(','.join([max_scale, max_scale]))
+            for i in coord:
                 url_template = url_template.replace(' ', i, 1)
             response = requests.get(url_template)
             if not response:
-                print('error')
+                print(f'error {response.text}')
             else:
                 self.map_file = "map.png"
                 with open(self.map_file, "wb") as file:
@@ -53,6 +57,7 @@ class Main(QMainWindow):
             self.label.setPixmap(pixmap)
 
     def closeEvent(self, event):
+        """"""
         try:
             os.remove(self.map_file)
         except TypeError:
@@ -61,16 +66,27 @@ class Main(QMainWindow):
     def keyPressEvent(self, key):
         if key.key() in [QtCore.Qt.Key_Enter, QtCore.Qt.Key_Return]:
             self.get_map()
-        if key.key() in [QtCore.Qt.Key_PageUp]:
+        if key.key() == QtCore.Qt.Key_PageUp:
             if self.scale < 90:
                 self.scale += 1
             self.lineEdit_2.setText(str(round(self.scale)))
             self.get_map()
-        if key.key() in [QtCore.Qt.Key_PageDown]:
+        if key.key() == QtCore.Qt.Key_PageDown:
             if self.scale > 0:
                 self.scale -= 1
             print(self.scale)
             self.lineEdit_2.setText(str(round(self.scale)))
+            self.get_map()
+        if key.key() == QtCore.Qt.Key_Up:
+            if int(self.position[1]) < 70:
+                self.position[1] += 0.5
+            print(self.position)
+            self.lineEdit.setText(' '.join(list(map(str, self.position))))
+            self.get_map()
+        if key.key() == QtCore.Qt.Key_Right:
+            #if int(self.position[0]) <
+            self.position[0] += 0.5
+            self.lineEdit.setText(' '.join(list(map(str, self.position))))
             self.get_map()
 
 
